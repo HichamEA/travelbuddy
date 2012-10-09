6caeebf15c0e905b2bc0546cb4e6b2c115c733ba
Bacancy <binal.patel@bacancytechnology.com>
2012-10-09 19:16:35 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelbuddy::Application.config.secret_token = '537926879678d0772a93de9cae04430cc8c8e5475c5456c190f4186b8b1ec04bd1ba1e6bde48ae25ed6006025c03cce4578d0ad723c89666f68500226e46da16'
