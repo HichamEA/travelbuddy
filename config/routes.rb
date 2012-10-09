cc6f44a073d731c206f496b9c660f40fd163ac58
Bacancy <binal.patel@bacancytechnology.com>
2012-10-09 21:05:17 +0530

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Travelbuddy::Application.routes.draw do
   
-  root :to => 'static_pages#home'
-  
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
-  get "static_pages/news"
+  get "users/new"
+
+  root to: 'static_pages#home'
+  match '/signup', to: 'Users#new'
+  #get "static_pages/help"
+  # get "static_pages/about"
+  # get "static_pages/contact"
+  # get "static_pages/news"
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/news', to: 'static_pages#news'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
