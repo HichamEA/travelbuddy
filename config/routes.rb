de49ec9ae91d011f3c7c17b2ac1f4d5d9d017c1c
Bacancy <binal.patel@bacancytechnology.com>
2012-10-09 19:37:32 +0530

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Travelbuddy::Application.routes.draw do
+  
+  root :to => 'static_pages#home'
+  
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
+  get "static_pages/news"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,6 @@ Travelbuddy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
